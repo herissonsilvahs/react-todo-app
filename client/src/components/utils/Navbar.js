c36cf34ad8aecfcfc3d2dc3e66b1ddcf987ab7ff
Herisson Silva <herissonsilvahs@gmail.com>
2018-10-12 13:31:39 -0300

    inicio client

---
+++ client/src/components/utils/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-inverse bg-inverse">
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">
+            <i className="fa fa-calendar-check-o"></i>
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
 import './assets/style.css'
 
 ReactDOM.render(<App />, document.getElementById('app'))
